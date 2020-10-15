107f37c2be51bc97893465ad8278cdeb5b365b98
vineethjayadevan <vineethjay1998@gmail.com>
2020-10-15 14:47:27 +0530

    The main method

--- MyApp/src/main/java/com/ofss/HelloWorld.java
+++ MyApp/src/main/java/com/ofss/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("first program in git");
+		Customer cust=new Customer("vineeth", "chennai");
+		System.out.println(cust.toString());
 
 	}
 
