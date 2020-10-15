c7fde9c9297da6f8d6aeff0fe64ed38300bcd3ea
vineethjayadevan <vineethjay1998@gmail.com>
2020-10-15 15:06:38 +0530

    Have updated into maven project(change made in pom.xml)

--- MyApp/src/main/java/com/ofss/Customer.java
+++ MyApp/src/main/java/com/ofss/Customer.java
@@ -3,5 +3,27 @@ package com.ofss;
 public class Customer {
 	private String name;
 	private String location;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	@Override
+	public String toString() {
+		return "Customer [name=" + name + ", location=" + location + "]";
+	}
+	public Customer(String name, String location) {
+		super();
+		this.name = name;
+		this.location = location;
+	}
+	
 
 }
